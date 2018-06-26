45d65198b2dc8300875b014856d0da83930b57d8
BirundhaPS <keyank588@gmail.com>
2018-06-26 16:31:55 -0400

    Changes made ICICI class

--- My First Project/src/com/hdfc/loans/carloans/ICICI.java
+++ My First Project/src/com/hdfc/loans/carloans/ICICI.java
@@ -3,8 +3,8 @@ package com.hdfc.loans.carloans;
 public class ICICI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Project from Eclipse to Github");
 	}
 
 }
