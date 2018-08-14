bcf63246d15485f149aafc8f2b45bffcc97b847e
BirundhaPS <keyank588@gmail.com>
2018-08-14 16:37:24 -0400

    Added main statement

--- My First Project/src/com/hdfc/loans/carloans/GitClass.java
+++ My First Project/src/com/hdfc/loans/carloans/GitClass.java
@@ -1,9 +1,10 @@
 package com.hdfc.loans.carloans;
 
 public class GitClass {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Github practice");
 
 	}
 
